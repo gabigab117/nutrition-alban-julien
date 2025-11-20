6cbb846463e8f54b7b7c38b6e7eb402fb2198efe
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-11-20 21:56:12 +0100

    feat: Implement password reset functionality and update navigation links

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,9 +16,25 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(
         next_page=reverse_lazy("home")
     ), name="logout"),
+    
+    # Change Password
     path("password_change", auth_views.PasswordChangeView.as_view(
         template_name="password/password_change.html",
         success_url=reverse_lazy("accounts:password_change_done")), name="password_change"),
     path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(
-        template_name="password/password_change_done.html"), name="password_change_done")
+        template_name="password/password_change_done.html"), name="password_change_done"),
+    
+    # Reset Password
+    path("password_reset/", auth_views.PasswordResetView.as_view(
+        template_name="password/password_reset.html",
+        email_template_name="email/password_reset_email.html",
+        success_url=reverse_lazy("accounts:password_reset_done")), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="password/password_reset_done.html"), name="password_reset_done"),
+    path("password_reset_confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="password/password_reset_confirm.html",
+        success_url=reverse_lazy("accounts:password_reset_complete")
+    ), name="password_reset_complete"),
+    path("password_reset/complete/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="password/password_change_done.html"), name="password_reset_complete")
 ]
--- project/urls.py
+++ project/urls.py
@@ -13,3 +13,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    
+    admin.site.site_header = "Docmachin Admin"
+    admin.site.site_title = "Docmachin Admin Portal"
+    admin.site.index_title = "Welcome to Docmachin Admin Portal"
